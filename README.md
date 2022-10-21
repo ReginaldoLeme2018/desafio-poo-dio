71b976b6edc06a251f34d5a331ecf8125f766d9f
Reginaldo Leme <mykepmbr@gmail.com>
2022-10-20 22:36:56 -0300

    Classes Mentoria e Curso criadas

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # desafio-poo-dio
 Projeto desafio POO do Bootcamp IFood
+
+Plus: Criado a formatação da data através do  DateTimeFormatter
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    Curso curso1 = new Curso();
+        curso1.setTitulo("Curso JAVA");
+        curso1.setDescricao("Descrição do curso JAVA");
+        curso1.setCargaHoraria(8);
+
+    Curso curso2 = new Curso();
+        curso2.setTitulo("Curso JAVASCRIPT");
+        curso2.setDescricao("Descrição do curso JAVASCRIPT");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+
+        mentoria.setTitulo("Mentoria JAVA");
+        mentoria.setDescricao("Descrição do curso de Mentoria");
+        mentoria.setData(LocalDate.now());
+
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
     }
 }
