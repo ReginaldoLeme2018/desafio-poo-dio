3019281d3e836f3b1cf215ff18b4a7a3c5c20efb
Reginaldo Leme <mykepmbr@gmail.com>
2022-10-20 23:07:59 -0300

    Classes Dev e Bootcamp criadas

--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,12 @@ import java.time.format.DateTimeFormatter;
 public class Main {
     public static void main(String[] args) {
 
-    Curso curso1 = new Curso();
+        Curso curso1 = new Curso();
         curso1.setTitulo("Curso JAVA");
         curso1.setDescricao("Descrição do curso JAVA");
         curso1.setCargaHoraria(8);
 
-    Curso curso2 = new Curso();
+        Curso curso2 = new Curso();
         curso2.setTitulo("Curso JAVASCRIPT");
         curso2.setDescricao("Descrição do curso JAVASCRIPT");
         curso2.setCargaHoraria(4);
@@ -22,7 +22,6 @@ public class Main {
         mentoria.setTitulo("Mentoria JAVA");
         mentoria.setDescricao("Descrição do curso de Mentoria");
         mentoria.setData(LocalDate.now());
-
 
         System.out.println(curso1);
         System.out.println(curso2);
--- src/br/com/dio/desafio/dominio/Mentoria.java
+++ src/br/com/dio/desafio/dominio/Mentoria.java
@@ -3,28 +3,14 @@ package br.com.dio.desafio.dominio;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-public class Mentoria {
+public class Mentoria extends Conteudo {
 
-    private String titulo;
-    private String descricao;
-    private LocalDate data;
+     private LocalDate data;
 
-    public String getTitulo() {
-        return titulo;
+    @Override
+    public double calcularXP() {
+        return XP_PADRAO +20d;
     }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
     public LocalDate getData() {
         return data;
     }
@@ -38,8 +24,8 @@ public class Mentoria {
     @Override
     public String toString() {
         return "Mentoria{" +
-                "titulo='" + titulo + '\'' +
-                ", descricao='" + descricao + '\'' +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
                 ", data=" + data.format(formatters) +
                 '}';
     }
